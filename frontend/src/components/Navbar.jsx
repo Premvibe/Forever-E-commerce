36d6e7e832e8500b8b5ed9bcc6b41599a6539e7d
Prem <prem7736k@gmail.com>
2025-06-16 21:12:11 +0530

    Navbar done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -63,11 +63,13 @@ const Navbar = () => {
             </div>
 
          </div>
-        <Link to="/cart">
-          <img src={assets.cart_icon} className='w-5 cursor-pointer' alt="" />
-        </Link>
-      </div>
+        <Link to="/cart " className='relative'>
+        
+        <img src={assets.cart_icon} className="w-5 min-w-5 cursor-pointer" alt="" />
+           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">10</p>
 
+        </Link>
+      </div> 
     </div>
   );
 };
