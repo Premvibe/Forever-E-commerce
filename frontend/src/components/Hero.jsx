e1ea1e51ed672f0c20e1cc4b4310a8da2b1b45db
Prem <prem7736k@gmail.com>
2025-06-19 18:13:29 +0530

    Hero section

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400 '>
+      {/* Left Section */}
+      <div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,9 @@ import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
+
+   
+
   return (
     <div className="flex items-center justify-between py-5 font-medium relative">
       {/* Logo */}
@@ -57,19 +60,63 @@ const Navbar = () => {
       </div>
 
       {/* Sidebar menu for Mobile screen */}
-      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all duration ${visible ? 'w-full' : 'w-0'}`}>
-        {/* Add mobile nav here if needed */}
-        <div className="flex flex-col text-gray-600"> 
-          <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
-            <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
-             <p>Back</p>
+   {/* Sidebar menu for small screens */}
+   <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden h-1000 bg-white ease-in duration-300
+             ${visible ? 'w-full' : 'w-0'}`}
+      >
+        <div className="flex flex-col text-gray-600 ">
+          <div
+            onClick={() => {
+              setVisible(false);
+            }}
+            className='flex items-center gap-4 p-3 cursor-pointer'
+          >
+            <img src={assets.dropdown_icon} alt="" className='h-4 rotate-180' />
+            <p className="font-semibold">Back</p>
           </div>
-          
-          <NavLink className='py-2 pl-6 border ' to="/">HOME</NavLink>
-          <NavLink className='py-2 pl-6 border ' to="/collection">COLLECTION</NavLink>
-          <NavLink className='py-2 pl-6 border ' to="/about">ABOUT</NavLink>
-          <NavLink className='py-2 pl-6 border ' to="/contact">CONTACT</NavLink>
-
+<div className='flex flex-col gap-2'>
+          <NavLink
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="py-2 pl-6 border"
+            to="/"
+          >
+            {' '}
+            HOME{' '}
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="py-2 pl-6 border"
+            to="/collection"
+          >
+            {' '}
+            COLLECTION{' '}
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="py-2 pl-6 border"
+            to="/about"
+          >
+            {' '}
+            ABOUT{' '}
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="py-2 pl-6 border"
+            to="/contact"
+          >
+            {' '}
+            CONTACT{' '}
+          </NavLink>
+          </div>
         </div>
       </div>
     </div>
