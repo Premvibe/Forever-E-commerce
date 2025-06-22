2f496f990be0c1d11c30a3b760b949102e9824f1
Prem <prem7736k@gmail.com>
2025-06-22 08:55:55 +0530

    best seller

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import LatestCollection from '../components/LatestCollection';
+import BestSeller from '../components/BestSeller';
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
 
 <Hero/>
 <LatestCollection/>
+<BestSeller/>
     </div>
   );
 };
