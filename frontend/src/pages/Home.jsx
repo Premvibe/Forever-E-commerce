3d263d19db21ab18f1cf2b886edbf26c6be730e9
Prem <prem7736k@gmail.com>
2025-06-23 08:05:52 +0530

    ourpolicies

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import LatestCollection from '../components/LatestCollection';
 import BestSeller from '../components/BestSeller';
+import OurPolicy from '../components/OurPolicy';
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
 <Hero/>
 <LatestCollection/>
 <BestSeller/>
+<OurPolicy/>
     </div>
   );
 };
