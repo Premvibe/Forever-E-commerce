cbe8d469c7e63fcaa22aedda3dfc5c5efe5f2218
Prem <prem7736k@gmail.com>
2025-06-26 07:55:28 +0530

    collection

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -2,6 +2,9 @@
  
  const Collection = () => {
   const{ products } = useContext(ShopContext);
+  const[showFilters, setShowFilters] = useState(false);
+
+
    return (
      <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
        
@@ -9,6 +12,11 @@
 
        <div className='min-w-60' >
         <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+
+          {/*Category Filter  */}
+          <div className={`order border-gray-300 pl-5 py-3 mt-6 ${showFilters ? '' : 'hidden'} sm:block`}>
+
+          </div>
 
        </div>
 
